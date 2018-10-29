ceeebde81aea31059b0cf9d923b476c69f51a5a4
email <s18011@std.it-college.ac.jp>
2018-10-29 12:44:03 +0900

    Initial commit

---
+++ js/invader.js
@@ -0,0 +1,17 @@
+phina.globalize();
+
+
+
+const SCREEN_WIDTH = 960;
+const SCREEN_HEIGHT = 640;
+
+phina.main(() => {
+    const app = GameApp({
+        title; "インベーダー",
+        fps: 60;
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
+    });
+
+    app.run();
+});
